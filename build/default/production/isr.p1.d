bdf3a69cac2d834b5fafd10b1012936eeb6b3c86
Vengadesh41 <vengadeshkrishnan41@gmail.com>
2026-07-07 17:05:08 +0530

    Delete build/default/production directory

--- build/default/production/isr.p1.d
+++
@@ -1,6 +0,0 @@
-build/default/production/isr.p1:  \
-isr.c  \
-main.h  \
-matrix_keypad.h  \
-timers.h  \
-clcd.h 
--- build/default/production/micro_oven.p1.d
+++
@@ -1,6 +0,0 @@
-build/default/production/micro_oven.p1:  \
-micro_oven.c  \
-main.h  \
-matrix_keypad.h  \
-timers.h  \
-clcd.h 
